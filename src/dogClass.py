8311a5476d71cd87ac7a7f57d4090831d501e7a7
x <emir.abel.manjarrez@gmail.com>
2016-08-05 23:40:11 -0600

    Comentarios de código

--- src/dogClass.py
+++ src/dogClass.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-#
-class Dog():
-	name = ''
-	def bark(self):
+class Dog():  # Definición de clase
+	name = ''  # <--- propiedad nombre
+	def bark(self):  # <-- metodo para ladrar.
 		print "{}, guau... guau... ".format(self.name)
 		print
 
-
-'''---------------------'''
+''' Instancia de clase perro Doqui '''
 perro1 = Dog()
 perro1.name = 'Doqui'
 
+''' Instancia de clase perro Firulaes '''
 perro2 = Dog()
 perro2.name = 'Firulaes'
 
+''' Perros ladran '''
 perro2.bark();
 perro1.bark();
