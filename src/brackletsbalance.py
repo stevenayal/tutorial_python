3bd94a1ad4c5eca79b2a3f5b6284e24317083acb
x <emir.abel.manjarrez@gmail.com>
2016-08-09 00:55:03 -0600

    Prueba

--- src/brackletsbalance.py
+++ src/brackletsbalance.py
@@ -1,24 +1,28 @@
 # -*- encoding: utf-8 -*-
 apertura = {')':'(', ']':'[','}':'{'}
 stack = []
-expresion = raw_input('ingrese la expresión:')
+
+def evalua(expresion):
+	error = False
+	for caracter in expresion:
+		if caracter in '([{':
+			stack.append(caracter)
+		elif caracter in ')]}':
+			caracter_apertura = apertura[caracter]
+			if not stack:
+				error = True
+				break
+			caracter_pila = stack.pop()
+			if caracter_apertura != caracter_pila:
+				error = True
+				break
+	return error
+
+expresion = raw_input('Ingrese la expresión:')
 if not expresion:
     print 'Error expresión no valida'
 
-error = False
-for caracter in expresion:
-	if caracter in '([{':
-		stack.append(caracter)
-	elif caracter in ')]}':
-		caracter_apertura = apertura[caracter]
-		if not stack:
-			error = True
-			break
-		caracter_pila = stack.pop()
-		if caracter_apertura != caracter_pila:
-			error = True
-			break
-if error:
+if evalua(expresion):
 	print 'Expresión no balanceada ', expresion
 else:
 	print 'Expresión Valida...', expresion
