e443bc74c10f05113db90913d81c67974ea21736
x <emir.abel.manjarrez@gmail.com>
2016-08-09 18:39:57 -0600

    Ejemplos de matematicas

---
+++ src/mathexample.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 *-*
+import math
+print 'Metodos de modulo Math'
+
+print "* floor(x).- El entero mas grande no mayor que x.  math.floor(5.9) => ", math.floor(5.9)
+print "* ceil(x) .- El entero mas pequeño  mayor que igual x.  math.ceil(5.5) => ", math.ceil(5.5)
+print "* log(x)  .- El logaritmo natural de x. X mayor a cero  math.log(11) => ", math.log(11)
+print "* log10(x).- El logaritmo base 10 de x. X mayor a cero  math.log10(14) => ", math.log10(14)
+print "* modf(x) .- Obtiene una tupla con los componentes de un numero real. math.modf(5.5) => ", math.modf(5.5)
+print "* pow(x,y) .- Obtiene el valor de x ** y. math.pow(2, 3) => ", math.pow(2, 3)
+print "* sqrt(x) .- Obtiene la raiz cuadrada de x. math.sqrt(16) => ", math.sqrt(16)
+
+print
+print 'Métodos predefinidos'
+print "* abs(x).-Valor absoluto de x.  abs(-5) => ", abs(-5)
+print "* max(*n).-El mayor elemento de la lista  max(1,4,5,6,8, 12) => ", max(1,4,5,6,8, 12)
+print "* min(*n).-El menor elemento de la lista  min(1,4,5,6,8, 12) => ", min(1,4,5,6,8, 12)
+print "* round(x,[y]) .- Redondea el numero x a y decimales round(0.5) => ", round(0.5)
+print "* 					  round(4.523, 2) => ", round(4.523, 2)
