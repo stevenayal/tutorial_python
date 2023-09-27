ba0f25662716dc882366be3ba50aeacc8aa67925
Steven Ayala <stevengilbertayala@gmail.com>
2023-09-27 19:34:40 -0400

    Correccion de Ejercicios de Operadores
    correccion del print y correccion de conversion de datos a float

--- src/operadores.py
+++ src/operadores.py
@@ -1,13 +1,13 @@
-subtotal = input('Ingrese el subtotal: ')
-porc_descuento = input('Ingrese el descuento: ')
-tasa_iva = input('Ingrese la  tasa de I.V.A: ')
+subtotal = float(input("Ingrese el subtotal: "))
+porc_descuento = float(input("Ingrese el descuento: "))
+tasa_iva = float(input("Ingrese la  tasa de I.V.A: "))
 
 descuento = subtotal * (porc_descuento / 100.00)
 subtotal_menos_descuento = subtotal - descuento
 importe_iva = subtotal_menos_descuento * (tasa_iva / 100.00)
 total = subtotal_menos_descuento + importe_iva
 
-print "========================="
-print 'Total: ', total
-print 'Descuento: ', descuento
-print 'IVA:', importe_iva
+print ("=========================")
+print ("Total: ", total)
+print ("Descuento: ", descuento)
+print ("IVA:", importe_iva)
